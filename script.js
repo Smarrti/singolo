363187636a2d83edd2682898ca47c551f850bb81
Sergey <bashkin2013@mail.ru>
2020-03-15 20:58:45 +0400

    feat: add fucntion for potfolio

--- script.js
+++ script.js
@@ -50,3 +50,39 @@ phoneHorizontal.addEventListener('click', () => {
         screenHorizontal.setAttribute('src', 'assets/phone/background-horizontal.png');
     }
 });
+
+const portfolioTags = document.querySelectorAll('.portfolio-tags li');
+const portfolioImages = document.querySelectorAll('.portfolio-images img');
+
+portfolioTags.forEach((e) => {
+    e.addEventListener('click', (event) => {
+        portfolioTags.forEach(e => {
+            e.classList.remove('portfolio-tags_active');
+        });
+        event.target.classList.add('portfolio-tags_active');
+        let random = [];
+        for (let i = 0; i < 12; i++) {
+            let randomNumber = Math.floor(Math.random() * Math.floor(12));
+            while(random.indexOf(randomNumber) !== -1) {
+                randomNumber = Math.floor(Math.random() * Math.floor(12));
+            }              
+            random.push(randomNumber);
+        }
+        let imagesBlock = document.querySelector('.portfolio-images');
+        while (imagesBlock.firstChild) {
+            imagesBlock.removeChild(imagesBlock.firstChild);
+        }
+        random.forEach((e) => {
+            imagesBlock.appendChild(portfolioImages[e]);
+        })
+    })
+})
+
+portfolioImages.forEach((e) => {
+    e.addEventListener('click', (event) => {
+        portfolioImages.forEach((e) => {
+            e.classList.remove('portfolio-image_bordered');
+        })
+        event.target.classList.add('portfolio-image_bordered');
+    })
+})
